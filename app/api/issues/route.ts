8de02c18fdc546a29532152e6ce7de9db8633d90
z0zero <briansangapta@gmail.com>
2024-01-27 20:54:24 +0700

    Handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,21 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
-import { z } from "zod";
+import { z } from 'zod';
 import prisma from "@/prisma/client";
 
-const createIssuesSchema = z.object({
-    title: z.string().min(3).max(100),
-    description: z.string().min(3).max(1000),
+const createIssueSchema = z.object({
+  title: z.string().min(1, 'Title is required.').max(255),
+  description: z.string().min(1, 'Description is required.')
 });
 
 export async function POST(request: NextRequest) {
-    const body  = await request.json();
-    const validation = createIssuesSchema.safeParse(body);
-    if (!validation.success) 
-        return NextResponse.json(validation.error.errors, { status: 400 });
-    
-    const newIssue = await prisma.issue.create({
-        data: {title: body.title, description: body.description},
-    });
+  const body = await request.json();
+  const validation = createIssueSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
-    return NextResponse.json(newIssue, { status: 201 });
+  const newIssue = await prisma.issue.create({
+    data: { title: body.title, description: body.description }
+  });
+
+  return NextResponse.json(newIssue, { status: 201 });
 }
